184e6db54823eaaa359053377c4ffa4216bf80fe
Daniel Jenkala <djenkala@yahoo.com>
2019-11-14 23:15:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,9 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
-  BRANDS = []
 
   def initialize(brand)
     @brand = brand
-  end
-  
-  def brand=(brand)
-    BRANDS << brand
   end
 
   def cobble
